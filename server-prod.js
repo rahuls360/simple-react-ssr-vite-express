6caf1e0eabf1d28aeaa588c9b2d79e7783a2f5fe
Rahul Makhija <rahuls360@gmail.com>
2024-04-21 10:18:18 +0530

    move from res.end to res.send

--- server-prod.js
+++ server-prod.js
@@ -1,28 +1,33 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
 
 const app = express();
 
-app.use(express.static(path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist/client'), { index: false }));
+app.use(
+	express.static(
+		path.resolve(path.dirname(fileURLToPath(import.meta.url)), "dist/client"),
+		{ index: false }
+	)
+);
 
-app.use('*', async (_, res) => {
-  try {
-    const template = fs.readFileSync('./dist/client/index.html', 'utf-8');
-    const { render } = await import('./dist/server/entry-server.js');
+app.use("*", async (_, res) => {
+	try {
+		const template = fs.readFileSync("./dist/client/index.html", "utf-8");
+		const { render } = await import("./dist/server/entry-server.js");
 
-    const { getServerData } = await import('./dist/function/function.js');
-    const data = await getServerData();
-    const script = `<script>window.__data__=${JSON.stringify(data)}</script>`;
+		const { getServerData } = await import("./dist/function/function.js");
+		const data = await getServerData();
+		const script = `<script>window.__data__=${JSON.stringify(data)}</script>`;
 
-    const html = template.replace(`<!--outlet-->`, `${render(data)} ${script}`);
-    res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
-  } catch (error) {
-    res.status(500).end(error);
-  }
+		const html = template.replace(`<!--outlet-->`, `${render(data)} ${script}`);
+		res.status(200).set({ "Content-Type": "text/html" }).end(html);
+	} catch (error) {
+		res.status(500).send({ error });
+	}
 });
 
 app.listen(5173, () => {
-  console.log('http://localhost:5173.');
+	console.log("http://localhost:5173.");
 });
